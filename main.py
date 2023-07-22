6d8ff8238bb0ff2d394e3604986d850a2806a710
Sergey <segairk@gmail.com>
2023-07-22 10:00:00 +0800

    Conversation object

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 import os
+import openai
 from dotenv import load_dotenv
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
+
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+
+class Conversation:
+    def __init__(self, model):
+        self.model = model
+        self.messages = []
+
+    def add_message(self, role, content):
+        self.messages.append({
+            'role': role, 'content': content
+        })
+
+    def create_conversation(self):
+        return openai.ChatCompletion.create(
+            model=self.model,
+            messages=self.messages
+        )
+
 
